b3f537fd3948ec1dcd8ea19777f4c2c544a1c389
maximmigood <maksim.mikheev@gmail.com>
2022-01-24 22:18:53 +0300

    homework #3 task1 create

--- Task7.py
+++ Task7.py
@@ -1,11 +1,1 @@
-def division(num1, num2):
-    try:
-        return num1 / num2
-    except ZeroDivisionError:
-        return f'Вы пытаетесь делить {num1} на 0. Деление на 0 невозможно.'
 
-
-
-print(division(3, 5))
-print(division(4, 0))
-print(division(2, 1))
