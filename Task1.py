0b7451a984cf1302646b7bb3089ff9ccf00d8c24
maximmigood <maksim.mikheev@gmail.com>
2022-01-24 22:34:35 +0300

    homework #3 task2 create

--- Task1.py
+++ Task1.py
@@ -1,8 +1,14 @@
 def division(num1, num2):
+    """ Возвращает частное от деления двух чисел
+
+    (number, number) -> number
+
+    """
     try:
         return num1 / num2
     except ZeroDivisionError:
         return f'Вы пытаетесь делить {num1} на 0. Деление на 0 невозможно.'
+
 
 print(division(3, 5))
 print(division(4, 0))
