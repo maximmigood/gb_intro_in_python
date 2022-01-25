58c9ef06b393a896a32b7ae404dcf941c21ad30b
maximmigood <maksim.mikheev@gmail.com>
2022-01-25 20:24:35 +0300

    homework #3 task6,7 create

--- Task7.py
+++ Task7.py
@@ -1,1 +1,22 @@
+def int_func(w):
+    """
+    Переводит в верхний регистр первую букву слова
+    :param w: str
+    :return: str
+    """
+    return w[0].upper() + w[1:]
+
+
+def long_int_func(s):
+    """
+    Переводит в верхний регистр все слова предложения
+    :param s: str
+    :return: str
+    """
+    l = []
+    for w in s.split():
+        l.append(int_func(w))
+    return ' '.join(l)
+
+print(long_int_func('brown fox jumps over the lazy dog'))
 
