81f859a97222f73a501742fb0a17af918c8c504d
maximmigood <maksim.mikheev@gmail.com>
2022-02-03 22:29:37 +0300

    Revert "Merge pull request #5 from maximmigood/Lesson5"
    This reverts commit cfad99ada2666eea5465a3db5dbf7171a7e1e29e, reversing
    changes made to 764afd9ee802328b5759f0267d6ed5f6a79f8128.

--- task2.py
+++
@@ -1,7 +0,0 @@
-with open('task2.txt') as file_obj:
-    f = file_obj.readlines()
-print(f'В файле {len(f)} строк')
-words = 0
-for s in f:
-    words += len(s.split())
-print(f'В файле {words} слов')
