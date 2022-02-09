16226dc074312bae8f923ae665c11bffcb2ede91
maximmigood <maksim.mikheev@gmail.com>
2022-02-09 22:21:58 +0300

    homework #7 task1.py change

--- task1.py
+++ task1.py
@@ -3,11 +3,15 @@ class Matrix:
         self.mtr = m
 
     def __str__(self):
-        res = ''.join([str(a)+'\n' for a in self.mtr])
+        res = ''
+        for a in self.mtr:
+            for e in a:
+                res += f'{e:<5}'
+            res = res + '\n'
         return res
 
     def __add__(self, m2):
-        res = [[self.mtr[i][j] + m2[i][j] for j in range(len(self.mtr[i]))] for i in range(len(self.mtr))]
+        res = [[self.mtr[i][j] + m2.mtr[i][j] for j in range(len(self.mtr[i]))] for i in range(len(self.mtr))]
         return Matrix(res)
 
 ma = Matrix([[1, 2], [4, 5]])
