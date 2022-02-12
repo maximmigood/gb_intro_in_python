2463704ddb3431353730068739d61c7b8edb30f9
maximmigood <maksim.mikheev@gmail.com>
2022-02-12 22:02:40 +0300

    homework #8 task6.py update

--- task6.py
+++ task6.py
@@ -67,9 +67,13 @@ class Warehouse:
         return f'{self.name}: [{self.__equipment}]'
 
 class OfficeEquipment:
-    def __init__(self, type, model):
-        self.model : str = model
-        self.type : str = type
+    def __init__(self, tp, model):
+        if not (type(model) == str):
+            raise TypeError('Parametr 1 [type] must be str')
+        if not (type(tp) == str):
+            raise TypeError('Parametr 2 [model] must be str')
+        self.model = model
+        self.type = tp
 
     def __eq__(self, other):
         return (self.type == other.type
@@ -84,18 +88,28 @@ class OfficeEquipment:
 
 class Printer(OfficeEquipment):
     def __init__(self, model, color=False):
+        if not type(model) == str:
+            raise TypeError('Parametr 1 [type] must be str')
+        if not type(color) == bool:
+            raise TypeError('Parametr 2 [color] must be bool')
         super().__init__('Принтер', model)
         self.color = color
 
 
 class Scanner(OfficeEquipment):
     def __init__(self, model, size='A4'):
+        if not type(model) == str:
+            raise TypeError('Parametr 1 [type] must be str')
+        if not type(size) == str:
+            raise TypeError('Parametr 2 [size] must be str')
         super().__init__('Сканер', model)
         self.size = size
 
 
 class Xerox(OfficeEquipment):
     def __init__(self, model):
+        if not type(model) == str:
+            raise TypeError('Parametr 1 [type] must be str')
         super().__init__('Ксерокс', model)
 
 
@@ -106,17 +120,16 @@ print(wh)
 print(wh_ec)
 print(wh_b)
 
-
 sc1 = Scanner('Brother S14')
+#sc2 = Scanner(14) # Вызывает ошибку
 wh.put(Xerox('Xerox Z25-12'), 10)
-#wh.put(Xerox('Xerox Z25-12'), '42')
-#wh.put('Xerox Z25-12', 10)
+#wh.put(Xerox('Xerox Z25-12'), '42') # Вызывает ошибку
+#wh.put('Xerox Z25-12', 10) # Вызывает ошибку
 wh.put(Printer('HP WhideColor 1785H', True), 5)
 wh.put(sc1, 2)
 print(wh)
 print(wh_ec)
 print(wh_b)
-
 
 wh.transfer_to(Xerox('Xerox Z25-12'), 3, wh_ec)
 wh.transfer_to(sc1, 1, wh_b)
